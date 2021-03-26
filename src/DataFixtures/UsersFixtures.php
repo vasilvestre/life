7395fbd4ffcd7f1cda4f6a8752b27190f4bdb9e3
vasilvestre <vsilvestre.pro@gmail.com>
2021-03-26 21:42:27 +0100

    Add scheduler for cron operation. File do not get logged :(

---
+++ src/DataFixtures/UsersFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+
+class UsersFixtures extends Fixture
+{
+
+private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+	$user->setEmail('admin');
+	$user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword($this->passwordEncoder->encodePassword(
+            $user,
+            'admin'
+        ));
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
