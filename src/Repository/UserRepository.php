e5720e57c8bc7d1aeeaa40a3c9fc7154e9a27d7b
Valentin Silvestre <vsilvestre.pro@gmail.com>
2021-09-14 14:43:41 +0200

    Fix strange template tag

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -14,7 +14,6 @@ use Symfony\Component\Security\Core\User\UserInterface;
  * @method User|null findOneBy(array $criteria, array $orderBy = null)
  * @method User[]    findAll()
  * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
- * @template User of object
  * @extends \Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository<User>
  */
 class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
