73bfaeeabbbf7448cd363334d5246991cd4c7f33
pro <vsilvestre.pro@gmail.com>
2021-09-14 15:56:26 +0200

    Use BrefKernel to avoid customization

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,12 +2,13 @@
 
 namespace App;
 
+use Bref\SymfonyBridge\BrefKernel;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
-class Kernel extends BaseKernel
+class Kernel extends BrefKernel
 {
     use MicroKernelTrait;
 
@@ -34,30 +35,5 @@ class Kernel extends BaseKernel
         } elseif (is_file($path = \dirname(__DIR__) . '/config/routes.php')) {
             (require $path)($routes->withPath($path), $this);
         }
-    }
-
-    public function getLogDir()
-    {
-        // When on the lambda only /tmp is writeable
-        if (isset($_SERVER['LAMBDA_TASK_ROOT'])) {
-            return '/tmp/log/';
-        }
-
-        return parent::getLogDir();
-    }
-
-    public function getCacheDir()
-    {
-        // When on the lambda only /tmp is writeable
-        if (isset($_SERVER['LAMBDA_TASK_ROOT'])) {
-            return '/tmp/cache/' . $this->environment;
-        }
-
-        return parent::getCacheDir();
-    }
-    
-    public function getBuildDir(): string
-    {
-        return $this->getProjectDir().'/var/cache/'.$this->environment;
     }
 }
